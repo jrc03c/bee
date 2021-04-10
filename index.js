2f233122910e58e98b3e9be3ad0da06ee6612862
jrc03c <jrc03c@pm.me>
2021-04-10 11:13:19 -0500

    ...

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
+class SubscriptionService {
+  constructor(){
+    let self = this
+    self.subscriptions = {}
+  }
+
+  on(path, callback){
+    let self = this
+    if (!self.subscriptions[path]) self.subscriptions[path] = []
+    self.subscriptions[path].push(callback)
+    return self
+  }
+
+  off(path, callback){
+    let self = this
+    self.subscriptions[path].splice(self.subscriptions[path].indexOf(callback), 1)
+    if (self.subscriptions[path].length === 0) delete self.subscriptions[path]
+    return self
+  }
+
+  run(path, payload){
+    let self = this
+    let callbacks = self.subscriptions[path]
+
+    if (callbacks){
+      callbacks.forEach(callback => {
+        callback(payload)
+      })
+    }
+
+    return self
+  }
+}
+
 if (typeof(window) !== "undefined"){
-  class Bee {
+  class Bee extends SubscriptionService {
     constructor(file){
+      super()
+
       let self = this
       self.worker = new Worker(file)
     }
 
-    do(path, payload){
+    run(path, payload){
       let self = this
-
-      self.worker.postMessage({
-        path,
-        payload,
-      })
-
-      return self
+      self.worker.postMessage({path, payload})
     }
   }
 
@@ -21,33 +51,7 @@ if (typeof(window) !== "undefined"){
 }
 
 if (typeof(module) !== "undefined"){
-  const bee = {
-    subscriptions: {},
-
-    on: function(path, callback){
-      let self = this
-      if (!self.subscriptions[path]) self.subscriptions[path] = []
-      self.subscriptions[path].push(callback)
-      return self
-    },
-
-    off: function(path, callback){
-      let self = this
-      self.subscriptions[path].splice(self.subscriptions[path].indexOf(callback), 1)
-      if (self.subscriptions[path].length === 0) delete self.subscriptions[path]
-      return self
-    },
-  }
-
-  onmessage = function(event){
-    let callbacks = bee.subscriptions[event.data.path]
-
-    if (callbacks){
-      callbacks.forEach(callback => {
-        callback(event.data.payload)
-      })
-    }
-  }
-
+  const bee = new SubscriptionService()
+  onmessage = (event) => bee.run(event.data.path, event.data.payload)
   module.exports = bee
 }
