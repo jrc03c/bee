336df99fcaea5749fd3957f2930b40e79a12c9da
jrc03c <jrc03c@pm.me>
2021-04-11 11:14:04 -0500

    ...

--- worker.js
+++ worker.js
@@ -1,37 +1,7 @@
-importScripts("subscription-service.js")
+importScripts("bee.js")
+const drone = new Bee.Drone()
 
-class Bee extends SubscriptionService {
-  constructor(){
-    super()
-    let self = this
-    onmessage = (event) => self.run(event.data.path, event.data.payload, event.data.cbid)
-  }
-
-  run(path, payload, cbid){
-    let self = this
-
-    let request = {
-      data: payload,
-    }
-
-    let response = {
-      send: function(result){
-        postMessage({
-          path: cbid,
-          payload: result,
-        })
-      },
-    }
-
-    let callbacks = self.subscriptions[path]
-    if (callbacks) callbacks.forEach(cb => cb(request, response))
-    return self
-  }
-}
-
-const bee = new Bee()
-
-bee.on("double", function(request, response){
+drone.on("double", function(request, response){
   let x = request.data
   return response.send(x * 2)
 })
